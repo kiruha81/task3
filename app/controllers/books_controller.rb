b3b23f187d0f5763c6a113cfcab68070c009e57e
EC2 Default User <ec2-user@ip-172-31-4-186.ap-northeast-1.compute.internal>
2023-01-24 12:34:21 +0000

    次はuser編集直す

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,30 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-
+  before_action :correct_user, only: [:edit, :update]
   def show
-    @book = Book.find(params[:id])
-    @user = @book.user
+    @bookid = Book.find(params[:id])
+    @users = @bookid.user
+    @user = current_user
+    @books = Book.new
   end
 
   def create
     @book = Book.new(book_params)
-    if @book.user_id = current_user.id
-      @book.save
+    @book.user_id = current_user.id
+    if  @book.save
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      @user = current_user
+      render :index
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    unless @book.user_id = current_user.id
+      redirect_to user_path(current_user.id)
+    end
     @books = Book.new
     @user = @books.user
   end
@@ -28,15 +32,18 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.user_id = current_user.id
+      @book.update(book_params)
     else
       redirect_to book_path
     end
-    @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path
   end
 
   def index
+    @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def destroy
@@ -49,5 +56,10 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
   end
 end
